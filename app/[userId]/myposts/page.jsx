659d394e9a07331eaf42e6282f394690343b019c
txxasif <txxasif@gmail.com>
2024-06-25 02:15:35 +0600

    updated

--- app/[userId]/myposts/page.jsx
+++ app/[userId]/myposts/page.jsx
@@ -3,7 +3,7 @@ import UserProperties from "./userposts";
 export default async function MyPosts({ params }) {
   console.log(params);
   const myPosts = await fetch(
-    `http:localhost:3000/api/property/user?id=${params.userId}`,
+    `${process.env.URL}/api/property/user?id=${params.userId}`,
     {
       cache: "no-store",
     }
@@ -11,7 +11,6 @@ export default async function MyPosts({ params }) {
     .then((res) => res.json())
     .then((data) => data.data);
 
-  console.log(myPosts);
   if (!myPosts) {
     return <h1>Loai</h1>;
   }
